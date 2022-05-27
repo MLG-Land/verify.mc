e328f6b8d503bba6631b220ad793c91c244a9583
BasicCorruption <saytleader@outlook.com>
2022-05-27 18:03:24 -0400

    Update VerifyMC.java

--- src/net/mlgland/verifymc/VerifyMC.java
+++ src/net/mlgland/verifymc/VerifyMC.java
@@ -1,17 +1,18 @@
 package net.mlgland.verifymc;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class VerifyMC extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[VerifyMC] Plugin has been enabled!");
     }
 
     @Override
     public void onDisable() {
-
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[VerifyMC] Plugin has been disabled!");
     }
 
 }
