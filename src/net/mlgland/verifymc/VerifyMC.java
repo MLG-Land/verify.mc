9da8e49e627ffef648c9dbbe1e6e6e506942efb7
BasicCorruption <saytleader@outlook.com>
2022-05-27 17:49:12 -0400

    Update VerifyMC.java

--- src/net/mlgland/verifymc/VerifyMC.java
+++ src/net/mlgland/verifymc/VerifyMC.java
@@ -1,5 +1,17 @@
 package net.mlgland.verifymc;
 
-public class VerifyMC {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VerifyMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 
 }
