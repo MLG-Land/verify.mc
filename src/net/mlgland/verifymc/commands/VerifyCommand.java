761ef0c25dc35a651610c23fc60cbcddb262a8f4
BasicCorruption <saytleader@outlook.com>
2022-05-28 15:51:00 -0400

    Update VerifyCommand.java

--- src/net/mlgland/verifymc/commands/VerifyCommand.java
+++ src/net/mlgland/verifymc/commands/VerifyCommand.java
@@ -5,6 +5,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.time.Instant;
 import java.util.Base64;
 
@@ -28,9 +30,23 @@ public class VerifyCommand implements CommandExecutor {
         String timestamp = String.valueOf(Instant.now().getEpochSecond());
         String combined = playerName + "," + playerUUID + "," + timestamp;
         String json = "{\"username\":\"" + playerName + "\",\"uuid\":\"" + playerUUID + "\",\"time\":\"" + timestamp + "\"" + "}";
-        String encodedParam = Base64.getEncoder().encodeToString(json.getBytes());
-        String resultURL = baseURL + encodedParam;
-        player.sendMessage("Click this link to verify you discord account: " + resultURL);
+//        String encodedParam = Base64.getEncoder().encodeToString(json.getBytes());
+        String hash = null;
+        try {
+            String secret = "why are you trying to hack us?";
+            String message = json;
+
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+
+            hash = Base64.getEncoder().encodeToString(sha256_HMAC.doFinal(message.getBytes()));
+            System.out.println(hash);
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        String resultURL = baseURL + hash;
+        player.sendMessage("Click this link to verify your discord account: " + resultURL);
 
         return true;
 
